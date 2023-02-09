d88356db78b6eff962a5f6727ebcffcb678219ba
Isaac Yauk <isaacyauk@yahoo.com>
2023-02-08 18:26:36 -0700

    Code Markup; adds comments and some TODOs

--- src/WordReader.java
+++ src/WordReader.java
@@ -7,6 +7,8 @@ public class WordReader
 
     public WordReader(String input)
     {
+        // Basic string checker logic
+        // TODO: Create a dictionary of known Chinese words and their Pinyin
         if (Objects.equals(input, "你好"))
         {
             System.out.println(input);
@@ -14,9 +16,9 @@ public class WordReader
         }
         else
         {
+            // TODO: craft this statement to take the chinese word, and then the Pinyin from the user, if unknown.
             System.out.println("Yeah I got no clue what dat is holmes.");
         }
-
     }
     public void checkString()
     {
