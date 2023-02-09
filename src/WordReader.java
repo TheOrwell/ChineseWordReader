ae9c4841667120952487dd4a0fa6f0b7952835c3
Isaac Yauk <isaacyauk@yahoo.com>
2023-02-09 08:00:41 -0700

    Code Refactor; mildly refactors code, playing around with some ideas and leaves TODOs

--- src/WordReader.java
+++ src/WordReader.java
@@ -16,13 +16,12 @@ public class WordReader
         // TODO: Create a dictionary of known Chinese words and their Pinyin
         if (Objects.equals(input, "你好"))
         {
-            System.out.println(input);
             System.out.println("Nǐ hǎo");
         }
         else
         {
             // TODO: craft this statement to take the chinese word, and then the Pinyin from the user, if unknown.
-            System.out.println("Yeah I got no clue what dat is holmes.");
+            System.out.println(" ? ");
         }
     }
     public void iterateString(String input)
@@ -30,6 +29,8 @@ public class WordReader
         for (int i = 0; i <= input.length()-1; i++)
         {
             System.out.print("   " + input.charAt(i));
+            // TODO: get the checkString method running it's logic here!
+            System.out.print(checkString(input));
         }
 
     }
