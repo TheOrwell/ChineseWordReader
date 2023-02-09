d8379fe8bed43f35ba33f3c1bcf699dd2b0a4035
Isaac Yauk <isaacyauk@yahoo.com>
2023-02-08 18:22:45 -0700

    Code Creation; writes starter code and outlines some ideas on where to take the project

--- src/WordReader.java
+++ src/WordReader.java
@@ -1,4 +1,25 @@
+import java.util.Objects;
+
 public class WordReader
 {
-    
+    // Instance fields
+    private String valToRead;
+
+    public WordReader(String input)
+    {
+        if (Objects.equals(input, "你好"))
+        {
+            System.out.println(input);
+            System.out.println("Nǐ hǎo");
+        }
+        else
+        {
+            System.out.println("Yeah I got no clue what dat is holmes.");
+        }
+
+    }
+    public void checkString()
+    {
+
+    }
 }
--- src/WordReaderDemo.java
+++ src/WordReaderDemo.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
+
 public class WordReaderDemo
 {
     public static void main(String[] args)
     {
+        // Creates a scanner, takes input, and inserts it into method
+        Scanner inputGrabber = new Scanner(System.in);
+        System.out.println("Try inputting a Chinese word, like 你好: ");
+        String userInput = inputGrabber.nextLine();
+        WordReader check1 = new WordReader(userInput);
+
+        check1.checkString();
 
     }
 }
